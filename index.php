0390ada6f217abeb33cc4765802957c38ce779c4
SyedAbdulrab <syedabdulrab1133@gmail.com>
2023-12-03 11:58:05 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<form method="post" action="" class="mt-3 ml-3">
+    <div class="form-group">
+        <label for="searchKeyword">Search Keyword:</label>
+        <input type="text" name="searchKeyword" id="searchKeyword" class="form-control" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
